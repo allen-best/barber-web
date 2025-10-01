eeb5eb262a7fdd89422436ec7ead70f9914957c1
Allen <ajbest91@gmail.com>
2025-09-30 23:24:53 -0400

    change picture url

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 // Import all images from assets/images folder
 const galleryImages = [
-  '/assets/images/mazi-image-1.jpeg',
-  '/assets/images/mazi-image-2.jpeg',
-  '/assets/images/mazi-image-3.jpeg',
-  '/assets/images/mazi-image-4.jpeg',
-  '/assets/images/mazi-image-5.jpeg',
-  '/assets/images/mazi-image-6.jpeg',
-  '/assets/images/mazi-image-7.jpeg',
-  '/assets/images/mazi-image-8.jpeg'
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-1.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-2.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-3.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-4.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-5.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-6.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-7.jpeg',
+  'https://dkduedwvufcf6nhq.public.blob.vercel-storage.com/mazi-image-8.jpeg'
 ];
 
 const ImageGallery = () => {
+
   return (
     <div id="gallery" className="w-full py-8">
       <h2 className="text-3xl font-bold mb-6 text-center" style={{ color: '#A0522D' }}>
